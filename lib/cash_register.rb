1d4902cc3214fb06a4428aa73750c481f9098c7d
Alex Merecka <merecka@gmail.com>
2019-04-08 01:17:20 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ class CashRegister
   def apply_discount
     if @discount != 0
       self.total = @total * ((100-@discount)/100)
-      binding.pry
+    #  binding.pry
       "After the discount, the total comes to $#{@@total}."
     else
       "There is no discount to apply."
